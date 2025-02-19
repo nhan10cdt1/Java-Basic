21c25e4a1214e9af8fedcf378ec4e1acdd67dd46
nhan10cdt1 <nhanbkdn1211@gmail.com>
2025-02-19 13:34:24 +0900

    Java silver 11

---
+++ Chapter05/Bai01/Customer.java
@@ -0,0 +1,12 @@
+package Chapter05.Bai01;
+
+public class Customer {
+    private String name; //
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    
+}
---
+++ Chapter05/Bai01/main.java
@@ -0,0 +1,8 @@
+package Chapter05.Bai01;
+
+public class main {
+    public static void main(String[] args)  {
+        Customer c = new Customer();
+        System.err.println(c.getName());
+    }
+}
